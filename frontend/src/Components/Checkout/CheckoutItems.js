61157d8a5a825c61e3f14c02591bae744597a413
Shengxin Chen <124843788+SeanXC@users.noreply.github.com>
2025-09-21 23:26:15 +0100

    feat: Add checkOut component

---
+++ frontend/src/Components/Checkout/CheckoutItems.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Checkout.css";
+import getSymbolFromCurrency from 'currency-symbol-map';
+
+function CheckoutItems(props) {
+    return (
+        <div>
+            <div style={{ border: "1px solid #E7E7E7", width: "95%", display:"flex", height: "250px", margin: "25px"}}>
+                <div style={{margin: "25px"}}>
+                    <img height="200px" src={props.definition.image} />
+                </div>
+                <div style={{ marginTop: "20px"}}>
+                    <div style={{fontSize: "20px"}} className="textgap">{props.definition.name}</div>
+                    <div style={{ fontWeight: "bold"}} className="textgap">{getSymbolFromCurrency('EUR')} {props.definition.price}</div>
+                    <div className="textgap">{props.definition.status}</div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CheckoutItems;
