3477500e0bcbc448862897bdf84400746607957a
Shengxin Chen <124843788+SeanXC@users.noreply.github.com>
2025-09-23 19:14:16 +0100

    fix: Fix the 'See more' link navigation function in the ad component

--- frontend/src/Components/AdvistisementFour/AdvistisementFour.js
+++ frontend/src/Components/AdvistisementFour/AdvistisementFour.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './AdvistisementFour.css';
 
 function AdvistisementFour(props) {
     return (
         <div className='advistisementOne_main'>
             <div className='advistisementOne_header '>
-                Up to 70% off | Shop now
+                Best Electronics Sale | Limited Time
             </div>
             <div className='advistisementOne_body'>
-                <img src='https://ik.imagekit.io/kci9fh3yl/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1758467021579' className='ad_fourImage' />
-                <img src='https://ik.imagekit.io/kci9fh3yl/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1758467021579' className='ad_fourImage' />
-                <img src='https://ik.imagekit.io/kci9fh3yl/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1758467021579' className='ad_fourImage' />   
-                <img src='https://ik.imagekit.io/kci9fh3yl/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1758467021579' className='ad_fourImage' />
+                <img src='https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' className='ad_fourImage' alt='Tablet' />
+                <img src='https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' className='ad_fourImage' alt='Headphones' />
+                <img src='https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' className='ad_fourImage' alt='Smartwatch' />   
+                <img src='https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' className='ad_fourImage' alt='Gaming Console' />
             </div>
             <div className='advistisementOne_footer'>
-                See more
+                <Link to="/display" style={{ textDecoration: 'underline', color: 'blue' }}>
+                    See more
+                </Link>
             </div>
         </div>
     );
--- frontend/src/Components/AdvistisementOne/AdvistisementOne.js
+++ frontend/src/Components/AdvistisementOne/AdvistisementOne.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './AdvistisementOne.css';
 
 function AdvistisementOne(props) {
     return (
         <div className='advistisementOne_main'>
             <div className='advistisementOne_header '>
-                Up to 70% off | Shop now
+                Latest Tech Deals | Free Shipping
             </div>
             <div className='advistisementOne_body'>
-                <img src='https://ik.imagekit.io/kci9fh3yl/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1758467021579' width='300px' />
+                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80' width='300px' alt='Smartphone' />
             </div>
             <div className='advistisementOne_footer'>
-                See more
+                <Link to="/display" style={{ textDecoration: 'underline', color: 'blue' }}>
+                    See more
+                </Link>
             </div>
         </div>
     );
